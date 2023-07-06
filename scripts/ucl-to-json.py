d57de800ab3f8844cd031eb495b7204c762694ed
Simon de Vlieger <cmdr@supakeen.com>
2023-07-06 17:18:54 +0200

    add yaml

--- scripts/ucl-to-json.py
+++ scripts/ucl-to-json.py
@@ -4,16 +4,18 @@ import json
 
 import ucl
 import toml
+import yaml
 
 
 def main():
     for path in pathlib.Path(".").glob("*.ucl"):
         name, _ = path.name.split(".", 1)
 
-        with open(path) as file_ucl, open(f"{name}.json", "w") as file_json, open(f"{name}.toml", "w") as file_toml:
+        with open(path) as file_ucl, open(f"{name}.json", "w") as file_json, open(f"{name}.toml", "w") as file_toml, open(f"{name}.yaml", "w") as file_yaml:
             data = ucl.load(file_ucl.read())
             json.dump(data, file_json, indent=2)
             toml.dump(data, file_toml)
+            yaml.dump(data, file_yaml)
 
     return 0
 
