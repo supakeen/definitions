9f5a1cf39340f7d8d86944ea5167d6dca71aea64
Simon de Vlieger <cmdr@supakeen.com>
2023-07-06 17:37:33 +0200

    move into subdirs

--- scripts/ucl-to-json.py
+++ scripts/ucl-to-json.py
@@ -11,7 +11,7 @@ def main():
     for path in pathlib.Path(".").glob("*.ucl"):
         name, _ = path.name.split(".", 1)
 
-        with open(path) as file_ucl, open(f"{name}.json", "w") as file_json, open(f"{name}.toml", "w") as file_toml, open(f"{name}.yaml", "w") as file_yaml:
+        with open(path) as file_ucl, open(f"json/{name}.json", "w") as file_json, open(f"toml/{name}.toml", "w") as file_toml, open(f"yaml/{name}.yaml", "w") as file_yaml:
             data = ucl.load(file_ucl.read())
             json.dump(data, file_json, indent=2)
             toml.dump(data, file_toml)
