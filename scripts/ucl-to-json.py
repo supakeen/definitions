bca1eca95353335ba17a9a4ffa8fd325c2b59e2a
Simon de Vlieger <cmdr@supakeen.com>
2023-07-06 17:17:48 +0200

    add toml conversion

--- scripts/ucl-to-json.py
+++ scripts/ucl-to-json.py
@@ -3,15 +3,17 @@ import pathlib
 import json
 
 import ucl
+import toml
 
 
 def main():
     for path in pathlib.Path(".").glob("*.ucl"):
         name, _ = path.name.split(".", 1)
 
-        with open(path) as file_ucl, open(f"{name}.json", "w") as file_json:
+        with open(path) as file_ucl, open(f"{name}.json", "w") as file_json, open(f"{name}.toml", "w") as file_toml:
             data = ucl.load(file_ucl.read())
             json.dump(data, file_json, indent=2)
+            toml.dump(data, file_toml)
 
     return 0
 
